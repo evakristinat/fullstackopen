9b00f57c19c1b6c3486bcb429777b477720b641a
Eeva Takanen <eevakristiinatakanen@gmail.com>
2021-09-26 11:40:31 +0300

    2.11

--- 2/puhelinluettelo/src/App.js
+++ 2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const Filter = ({ search, handleSearchChange }) => {
@@ -46,16 +47,16 @@ const Persons = ({ personsToShow }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
-  
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -112,7 +113,6 @@ const App = () => {
 
       <h3>Numbers</h3>
       <Persons personsToShow={personsToShow} />
-
     </main>
   );
 };
